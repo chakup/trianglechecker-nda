cc5344a531978c1d460b903b584266b69ba1154a
Jakup Karagoz <jakar19@student.sdu.dk>
2022-05-24 13:58:08 +0200

    Added backend

---
+++ backend/src/main/java/dk/nordea/onedigital/trianglechecker/TrianglecheckerApplication.java
@@ -0,0 +1,13 @@
+package dk.nordea.onedigital.trianglechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianglecheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianglecheckerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dk/nordea/onedigital/trianglechecker/TrianglecheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.nordea.onedigital.trianglechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrianglecheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
